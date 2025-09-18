09264ca3eee50a920d2cd2a817f02f6b33fa046b
upendrag26 <upendrareddyg1@gmail.com>
2025-09-18 08:45:38 +0530

    added example variable

---
+++ 03_variable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PERSION1="Ramesh"
+PERSION2="Suresh"
+
+echo "$PERSION1: Hi $PERSION2, how are you?"
+echo "$PERSION2: I'm good, thanks! How about you?"     
+echo "$PERSION1: I'm doing well too. What have you been up to lately?"
+echo "$PERSION2: Just working on some projects and spending time with family. How about you?"
+echo "$PERSION1: Same here. I've also started learning a new hobby - photography."
+echo "$PERSION2: That sounds interesting! What kind of photography are you into?"
+echo "$PERSION1: Mostly nature and landscape photography. It's been a lot of fun exploring the outdoors."
+echo "$PERSION2: That's great! Maybe you can share some of your photos with me sometime."
+echo "$PERSION1: Definitely! I'd love to. Let's catch up again soon."
+echo "$PERSION2: Absolutely! Take care, $PERSION1."
