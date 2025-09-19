a4588b786e83c6a556b611433d2fdc060286ec64
upendrag26 <upendrareddyg1@gmail.com>
2025-09-19 07:31:49 +0530

    variable input from command

---
+++ 04_variaable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSION1=$1
+PERSION2=$2
+
+echo "$PERSION1: Hi $PERSION2, how are you?"
+echo "$PERSION2: I'm good, ra How about you?"     
+echo "$PERSION1: I'm doing well too. What have you been up to lately?"
+echo "$PERSION2: Just working on some projects and spending time with family. How about you?"
