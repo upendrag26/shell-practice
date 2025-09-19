f3e8cfc033ce16e3e17b68b4960945c48aa9029b
upendrag26 <upendrareddyg1@gmail.com>
2025-09-19 08:04:52 +0530

    date variable

--- 07-variable-date.sh
+++ 07-variable-date.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 # Pint the date and time
-Date=$(date)
-echo "Current Date and Time: $Date"
+#Date=$(date)
+START_TIME=$(date +%s)
+echo "Start Date and Time: $START_TIME"
+
+sleep 10
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+
+echo "Toatal Time: $TOTAL_TIME"
