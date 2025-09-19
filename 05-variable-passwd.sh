fe3f24c71626d0ab00f28b47fb1f701d1576e4ea
upendrag26 <upendrareddyg1@gmail.com>
2025-09-19 07:38:47 +0530

    variable read updated

--- 05-variable-passwd.sh
+++ 05-variable-passwd.sh
@@ -7,5 +7,6 @@ echo "The pin number you entered is: $pin"
 #input cannot be viewed when typed
 read -s -p "Enter your secret pin: " secretpin
 echo
-echo "The secret pin you entered is: $secretpin
+echo "The secret pin you entered is: $secretpin"
+
 
