9a3fcbd8c696f72940ba3cb2eb09e182b34d1d21
upendrag26 <upendrareddyg1@gmail.com>
2025-09-19 08:34:03 +0530

    list variable

--- 10-list_variable.sh
+++ 10-list_variable.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-LEADERS = ("Alice" "Bob" "Charlie" "Diana")
+LEADERS=("Alice" "Bob" "Charlie" "Diana")
 echo "Team Leaders: ${LEADERS[@]}"
+echo "First Leader: ${LEADERS[0]}"
+echo "Total Leaders: ${#LEADERS[@]}"
+echo "Index of Last Leader: $((${#LEADERS[@]} - 1))"
+echo "All Indices: ${!LEADERS[@]}"
